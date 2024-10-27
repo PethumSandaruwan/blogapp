6da1fb025604c851d66ee88866bfed72451f7acf
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-10-27 21:32:22 +0530

    updated route.js

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,5 +1,7 @@
 import { ConnectDB } from "@/lib/config/db"
+import BlogModel from "@/lib/models/blogModel";
 import{writeFile} from 'fs/promises'
+import { title } from "process";
 
 const { NextResponse } = require("next/server")
 
@@ -25,6 +27,17 @@ export async function POST(request) {
     const path=`./public/${timestamp}_${image.name}`
     await writeFile(path,buffer);
     const imgUrl=`/${timestamp}_${image.name}`
-    console.log(imgUrl);
-    return NextResponse.json({imgUrl})
+   
+const blogData = {
+    title:`${formData.get('title')}`,
+    description:`${formData.get('description')}`,
+    category:`${formData.get('category')}`,
+    author:`${formData.get('author')}`,
+    image:`${imgUrl}`,
+    authorImg:`${formData.get('authorImg')}`
+}
+await BlogModel.create(blogData);
+console.log("Blog Saved")
+
+    return NextResponse.json({success:true,msg:"Blog Added"})
 }
